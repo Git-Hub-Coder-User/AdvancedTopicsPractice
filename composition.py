9410fe1078f445fab02302bf1c92aaf22963d695
Git-Hub-Coder-user <elsa.schultheiss@ucas-edu.net>
2024-11-05 11:38:22 -0700

    Does pushing work?

---
+++ composition.py
@@ -0,0 +1,4 @@
+#Composition is a type of class that exists inside of another class (class has a other class)
+    #Is a relation or has a relationship
+    #A vampire is a monster: Yes, Inheritance
+    #Does a vampire have a monster: No
